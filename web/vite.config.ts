1264460ba3c88d3607d66b5f0791963fda10df28
creativebrandon <cre8tivebreed@gmail.com>
2024-04-03 22:13:07 -0400

    ci(gh-actions): added release and publish actions

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    https: {
+const getCerts = () => {
+  try {
+    return {
       key: fs.readFileSync('.ssl/key.pem'),
       cert: fs.readFileSync('.ssl/cert.pem')
     }
+  } catch {
+    return undefined
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: getCerts()
   },
-  plugins: [react()],
+  plugins: [react()]
 })
